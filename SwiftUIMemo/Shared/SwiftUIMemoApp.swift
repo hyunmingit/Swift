31588fb99a1b9633b2405068399176f168def8d7
user <jhmcos13@gmail.com>
2022-06-07 17:48:07 +0900

    step1

---
+++ SwiftUIMemo/Shared/SwiftUIMemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIMemoApp.swift
+//  Shared
+//
+//  Created by user on 2022/06/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            MainListView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
